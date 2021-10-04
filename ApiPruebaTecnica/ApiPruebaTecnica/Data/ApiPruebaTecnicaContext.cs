407ee9c8334e513d328bc662935dc9d6de6ecd88
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-10-03 23:34:37 -0300

    Avance 1
    Avance de dos proyectos, uno con C# para crear APIs y poder consumirlas con el segundo proyecto con REACT para crear un CRUD

---
+++ ApiPruebaTecnica/ApiPruebaTecnica/Data/ApiPruebaTecnicaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiPruebaTecnica.Models;
+
+namespace ApiPruebaTecnica.Data
+{
+    public class ApiPruebaTecnicaContext : DbContext
+    {
+        public ApiPruebaTecnicaContext (DbContextOptions<ApiPruebaTecnicaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiPruebaTecnica.Models.Persona> Persona { get; set; }
+    }
+}
---
+++ ApiPruebaTecnica/ApiPruebaTecnica/Models/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPruebaTecnica.Models
+{
+    public class Persona
+    {
+		public Guid Id { get; set; }
+		public int RunCuerpo { get; set; }
+		public string RunDigito { get; set; }
+		public string Nombres { get; set; }
+		public string ApellidoPaterno { get; set; }
+		public string? ApellidoMaterno { get; set; }
+		public string? Email { get; set; }
+		public short SexoCodigo { get; set; }
+		public DateTime? FechaNacimiento { get; set; }
+		public short? RegionCodigo { get; set; }
+		public short? CiudadCodigo { get; set; }
+		public short? ComunaCodigo { get; set; }
+		public string? Direccion { get; set; }
+		public int? Telefono { get; set; }
+		public string? Observaciones { get; set; }
+
+
+	}
+}
